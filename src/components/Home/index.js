ac597d78e3009166965d4378dec93e011597cc54
matheusned15 <matheusned15@gmail.com>
2021-02-26 00:47:44 -0300

    Criando componentes

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+// import React, {Component} from 'react';
+
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Tela Home</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
